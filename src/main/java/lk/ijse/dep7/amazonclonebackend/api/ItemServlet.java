c3d1c9e124cbe5254069f355d4fea962e22ac208
dasith <dasiththisaru@gmail.com>
2022-02-11 07:27:31 +0530

    Refactor the boilerplate servlet code to ItemServlet and inject the c…

--- src/main/java/lk/ijse/dep7/amazonclonebackend/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep7/amazonclonebackend/api/ItemServlet.java
@@ -1,15 +1,33 @@
 package lk.ijse.dep7.amazonclonebackend.api;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/item-servlet")
+import javax.sql.DataSource;
+
+@WebServlet(name = "helloServlet", value = "/item-servlet", loadOnStartup = 0)
 public class ItemServlet extends HttpServlet {
+
+    @Resource(name= "java:comp/env/jdbc/amazon")
+    private DataSource dataSource;
 
 
     public void init() {
+
+        try {
+            Connection con = dataSource.getConnection();
+            System.out.println(con);
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
