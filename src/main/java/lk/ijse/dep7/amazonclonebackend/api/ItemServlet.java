afc4dcd57061aee07d49d7b0f202a764b13f3813
dasith <dasiththisaru@gmail.com>
2022-02-11 07:42:06 +0530

    Finalize doGet() of ItemServlet

--- src/main/java/lk/ijse/dep7/amazonclonebackend/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep7/amazonclonebackend/api/ItemServlet.java
@@ -3,11 +3,16 @@ package lk.ijse.dep7.amazonclonebackend.api;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import jakarta.annotation.Resource;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import lk.ijse.dep7.amazonclonebackend.dto.ItemDTO;
+import lk.ijse.dep7.amazonclonebackend.service.ItemService;
 
 import javax.sql.DataSource;
 
@@ -20,19 +25,33 @@ public class ItemServlet extends HttpServlet {
 
     public void init() {
 
-        try {
+       /* try {
             Connection con = dataSource.getConnection();
             System.out.println(con);
             con.close();
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }*/
 
 
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        try {
+            Connection con = dataSource.getConnection();
+            ItemService itemService = new ItemService(con);
+
+            List<ItemDTO> allItems = itemService.getAllItems();
+            Jsonb jsonb = JsonbBuilder.create();
+            String s = jsonb.toJson(allItems);
+            response.setContentType("application/json");
+            PrintWriter writer = response.getWriter();
+            writer.println(s);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to obtain a new connection", e);
+        }
 
 
     }
